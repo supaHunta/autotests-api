83606f2dc2c1de6db0e4743fdc37909342726c72
supaHunta <super.neyt@yandex.ru>
2026-06-17 10:53:31 +0300

    Знакомство с Pydantic.Практика написания Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,10 +1,14 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
+from pydantic.alias_generators import to_camel
 
 
 class UserSchema(BaseModel):
     """
     Описание структуры пользователя
     """
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+
     id: str
     email: EmailStr
     last_name: str = Field(alias='lastName')
@@ -17,6 +21,8 @@ class CreateUserRequestSchema(BaseModel):
     """
     Описание структуры запроса на создание пользователя
     """
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
     email: EmailStr
     password: str
     last_name: str = Field(alias='lastName')
