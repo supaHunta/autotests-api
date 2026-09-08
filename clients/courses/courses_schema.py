d85d6ef979c987e6cfdc957eb5bf2c11ff09d54a
supaHunta <super.neyt@yandex.ru>
2026-09-08 10:34:59 +0300

    Практикуемся в написании API автотестов. Часть 2 .Практика написания API автотестов для работы с курсами

--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -74,3 +74,17 @@ class UpdateCourseRequestSchema(BaseModel):
     description: str | None = Field(default_factory=fake.text)
     # Добавили генерацию случайного предполагаемого времени прохождения курса
     estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+    # Остальной код без изменений
+
+class UpdateCourseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления курса.
+    """
+    course: CourseSchema
+
+class GetCoursesResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на получение списка курсов.
+    """
+    courses: list[CourseSchema]
