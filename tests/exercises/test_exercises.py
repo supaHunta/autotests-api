1a2f4d6ac081d9c90a721ee02be3c90bd09bc259
supaHunta <super.neyt@yandex.ru>
2026-09-16 14:39:26 +0300

    Практикуемся в написании API автотестов. Часть 3 .Практика написания API автотестов для работы с заданиями. Обновление задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -4,11 +4,12 @@ import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    GetExercisesQuerySchema, GetExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
+    assert_update_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -41,3 +42,18 @@ class TestExercises:
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_get_exercise_response(response_data, function_exercise.response)
         validate_json_schema(response.json(), GetExerciseResponseSchema.model_json_schema())
+
+    def test_patch_exercise(
+            self,
+            exercises_client: ExercisesClient,
+            function_exercise: ExerciseFixture):
+
+        request = UpdateExerciseRequestSchema()
+        response = exercises_client.update_exercise_api(
+            exercise_id=function_exercise.response.exercise.id,
+            request=request)
+        response_data = UpdateExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_update_exercise_response(request=request, response=response_data)
+        validate_json_schema(response.json(), UpdateExerciseResponseSchema.model_json_schema())
